c8bc6ede641af4057759bfe42eebce654e97de64
Mamoun77 <mamounhussam1000@gmail.com>
2026-01-08 12:25:23 +0300

    Add the ocr functionality to extract text from images and connect validation routes

--- agents_nodes/clear_valid_input_validator/input_validator.py
+++ agents_nodes/clear_valid_input_validator/input_validator.py
@@ -32,25 +32,64 @@ system_prompt = f"""
     - Note (important): for the attachments to be valid they all must be valid if one is invalid then all of them are invalid (ATTACHMENT_NOT_VALID)
 """
 
+system_prompt_text_only = f"""
+You are a medical text validator. Classify the input into ONE label.
+
+
+TEXT_VALID
+- Text shows medical intent (symptoms, diagnosis, treatment, test results, health questions)
+
+TEXT_NOT_VALID
+- Text is non-medical (greetings, jokes, coding, sports, general chat)
+
+Rules:
+- Don't be too strict, classify it as not valid only if the input is very clearly not a midical input
+- Output exactly one label: TEXT_VALID or TEXT_NOT_VALID
+
+Note: you will recieve a title for the text, take it as a small hint
+Note: the input could be an extracted text from an image using an ocr system or could be extracted from attachments (pdf, csv, etc...)
+"""
+
+
 
 class MedicalInputCheck(TypedDict):
     input_classification: Literal["TEXT_VALID_ATTACHMENT_VALID", "TEXT_VALID_ATTACHMENT_NOT_VALID", "TEXT_NOT_VALID_ATTACHMENT_VALID", "TEXT_NOT_VALID_ATTACHMENT_NOT_VALID"]
 
+class TextOnlyInputValidator(TypedDict):
+    input_classification: Literal["TEXT_VALID", "TEXT_NOT_VALID"]
+
 
 structured_llm = ChatGoogleGenerativeAI(
-    model="gemini-3-flash-preview",
+    model="gemini-2.5-flash-lite-preview-09-2025",
     google_api_key=os.getenv("GOOGLE_API_KEY"),
     
     ).with_structured_output(MedicalInputCheck)
 
+structured_llm_text_only = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash-lite-preview-09-2025",
+    google_api_key=os.getenv("GOOGLE_API_KEY"),
+    
+    ).with_structured_output(TextOnlyInputValidator)
 
-def validate_medical_input_agent(input_text: str, available_attachments=None):
+
+def validate_first_input(input_text: str, available_attachments=None):
 
     available_attachments = available_attachments if available_attachments else "The user did not provide any attachments."
 
     result = structured_llm.invoke([
         ("system", system_prompt),
         ("human", f"Text: {input_text}\nAttachments: {available_attachments}"),
+        
+    ])
+
+    return result['input_classification']
+
+def validate_input_text_only(title, input_text: str):
+
+
+    result = structured_llm_text_only.invoke([
+        ("system", system_prompt_text_only),
+        ("human", f"Title: {title}, Input Text: {input_text}"),
         
     ])
 
